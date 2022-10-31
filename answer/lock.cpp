87b6f93443fd5b42b1ae1e08a111b9d0e075484f
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-10-31 14:36:27 -0700

    Runned clang-format on every files in answer directory.

--- answer/lock.cpp
+++ answer/lock.cpp
@@ -1,9 +1,9 @@
 #include "Lock.h"
-#include <stddef.h>
 #include <Logger.h>
-#include "thread_lock.h"
+#include <stddef.h>
 #include "List.h"
 #include "Map.h"
+#include "thread_lock.h"
 
 // See os_simulator/includes/Lock.student.h for explanations of these functions
 // Not all of these functions must be implemented
@@ -18,64 +18,68 @@ void lockCreated(const char* lockId) {
 
 void lockAttempted(const char* lockId, Thread* thread) {
     char line[1024];
-    sprintf(line, "[lockAttempted] thread %s is attempting lock %s\n", thread->name, lockId);
+    sprintf(line, "[lockAttempted] thread %s is attempting lock %s\n",
+            thread->name, lockId);
     verboseLog(line);
 
-    const char * waitingThreadList = (const char *) GET_FROM_MAP(const char *, lockToWaitingThreads, lockId);
+    const char* waitingThreadList =
+        (const char*)GET_FROM_MAP(const char*, lockToWaitingThreads, lockId);
     addToList(waitingThreadList, thread);
     sortList(waitingThreadList, priorityCompare);
     removeFromList(readyList, 0);
 
     // PUT_IN_MAP(Thread*, threadToWaitingLock, thread, (void*)lockId);
 
-    Thread* holdingThread = (Thread*) GET_FROM_MAP(const char *, lockToHoldingThread, lockId);
+    Thread* holdingThread =
+        (Thread*)GET_FROM_MAP(const char*, lockToHoldingThread, lockId);
     if (!holdingThread) {
-        sprintf(line, "[lockAttempted] thread %s is attempting lock %s and the lock is not held by any thread\n", thread->name, lockId);
+        sprintf(line,
+                "[lockAttempted] thread %s is attempting lock %s and the lock "
+                "is not held by any thread\n",
+                thread->name, lockId);
         verboseLog(line);
     } else {
         if (holdingThread->priority < thread->priority) {
-            sprintf(line, "[lockAttempted] thread %s is attempting lock %s and it need to donate priority to thread %s\n", thread->name, lockId, holdingThread->name);
+            sprintf(line,
+                    "[lockAttempted] thread %s is attempting lock %s and it "
+                    "need to donate priority to thread %s\n",
+                    thread->name, lockId, holdingThread->name);
             verboseLog(line);
             holdingThread->priority = thread->priority;
             sprintf(
                 line,
-                "[lockAttempted] thread %s just donated priority to thread %s. Now thread %s's priority is %d, thread %s's priority is %d\n",
-                thread->name,
-                holdingThread->name,
-                thread->name,
-                thread->priority,
-                holdingThread->name,
-                holdingThread->priority
-            );
+                "[lockAttempted] thread %s just donated priority to thread %s. "
+                "Now thread %s's priority is %d, thread %s's priority is %d\n",
+                thread->name, holdingThread->name, thread->name,
+                thread->priority, holdingThread->name, holdingThread->priority);
             verboseLog(line);
-            bool isHoldingThreadBlocked = MAP_CONTAINS(Thread*, threadToWaitingLock, holdingThread);
+            bool isHoldingThreadBlocked =
+                MAP_CONTAINS(Thread*, threadToWaitingLock, holdingThread);
             if (isHoldingThreadBlocked) {
-                sprintf(
-                    line,
-                    "[lockAttempted] holdingThread is blocked, priority donation is required\n"
-                );
+                sprintf(line,
+                        "[lockAttempted] holdingThread is blocked, priority "
+                        "donation is required\n");
                 verboseLog(line);
-                const char * blockHoldingThreadLock = (const char *) GET_FROM_MAP(Thread*, threadToWaitingLock, holdingThread);
-                Thread* blockHoldingThreadThread = (Thread*) GET_FROM_MAP(const char *, lockToHoldingThread, blockHoldingThreadLock);
+                const char* blockHoldingThreadLock = (const char*)GET_FROM_MAP(
+                    Thread*, threadToWaitingLock, holdingThread);
+                Thread* blockHoldingThreadThread = (Thread*)GET_FROM_MAP(
+                    const char*, lockToHoldingThread, blockHoldingThreadLock);
                 blockHoldingThreadThread->priority = thread->priority;
-                sprintf(
-                    line,
-                    "[lockAttempted] nested priority donatoin - thread %s is attempting lock %s and it need to donate priority to thread %s\n",
-                    holdingThread->name,
-                    blockHoldingThreadLock,
-                    blockHoldingThreadThread->name
-                );
+                sprintf(line,
+                        "[lockAttempted] nested priority donatoin - thread %s "
+                        "is attempting lock %s and it need to donate priority "
+                        "to thread %s\n",
+                        holdingThread->name, blockHoldingThreadLock,
+                        blockHoldingThreadThread->name);
                 verboseLog(line);
-                sprintf(
-                    line,
-                    "[lockAttempted] nested priority donatoin - thread %s just donated priority to thread %s. Now thread %s's priority is %d, thread %s's priority is %d\n",
-                    holdingThread->name,
-                    blockHoldingThreadThread->name,
-                    holdingThread->name,
-                    holdingThread->priority,
-                    blockHoldingThreadThread->name,
-                    blockHoldingThreadThread->priority
-                );
+                sprintf(line,
+                        "[lockAttempted] nested priority donatoin - thread %s "
+                        "just donated priority to thread %s. Now thread %s's "
+                        "priority is %d, thread %s's priority is %d\n",
+                        holdingThread->name, blockHoldingThreadThread->name,
+                        holdingThread->name, holdingThread->priority,
+                        blockHoldingThreadThread->name,
+                        blockHoldingThreadThread->priority);
             }
             sortList(readyList, priorityCompare);
         }
@@ -86,7 +90,8 @@ void lockAttempted(const char* lockId, Thread* thread) {
 
 void lockAcquired(const char* lockId, Thread* thread) {
     char line[1024];
-    sprintf(line, "[lockAcquired] thread %s acquired lock %s\n", thread->name, lockId);
+    sprintf(line, "[lockAcquired] thread %s acquired lock %s\n", thread->name,
+            lockId);
     verboseLog(line);
     PUT_IN_MAP(const char*, lockToHoldingThread, lockId, (void*)thread);
     REMOVE_FROM_MAP(Thread*, threadToWaitingLock, thread);
@@ -94,27 +99,31 @@ void lockAcquired(const char* lockId, Thread* thread) {
 
 void lockFailed(const char* lockId, Thread* thread) {
     char line[1024];
-    sprintf(line, "[lockFailed] thread %s failed to acquire lock %s\n", thread->name, lockId);
+    sprintf(line, "[lockFailed] thread %s failed to acquire lock %s\n",
+            thread->name, lockId);
     verboseLog(line);
 
     removeFromList(readyList, (void*)thread);
     PUT_IN_MAP(Thread*, threadToWaitingLock, thread, (void*)lockId);
-    const char * waitingThreadList = (const char *) GET_FROM_MAP(const char *, lockToWaitingThreads, lockId);
+    const char* waitingThreadList =
+        (const char*)GET_FROM_MAP(const char*, lockToWaitingThreads, lockId);
     addToList(waitingThreadList, thread);
     sortList(waitingThreadList, priorityCompare);
 }
 
 void lockReleased(const char* lockId, Thread* thread) {
     char line[1024];
-    sprintf(line, "[lockReleased] thread %s released lock %s\n", thread->name, lockId);
+    sprintf(line, "[lockReleased] thread %s released lock %s\n", thread->name,
+            lockId);
     verboseLog(line);
     thread->priority = thread->originalPriority;
     sortList(readyList, priorityCompare);
-    PUT_IN_MAP(const char*, lockToHoldingThread, lockId, (void*) NULL);
+    PUT_IN_MAP(const char*, lockToHoldingThread, lockId, (void*)NULL);
 
-    const char * waitingThreadList = (const char *) GET_FROM_MAP(const char *, lockToWaitingThreads, lockId);
+    const char* waitingThreadList =
+        (const char*)GET_FROM_MAP(const char*, lockToWaitingThreads, lockId);
     for (int i = 0; i < listSize(waitingThreadList); i++) {
-        Thread* t = (Thread *) listGet(waitingThreadList, i);
+        Thread* t = (Thread*)listGet(waitingThreadList, i);
         REMOVE_FROM_MAP(Thread*, threadToWaitingLock, t);
     }
 }
@@ -124,7 +133,7 @@ void lockReleased(const char* lockId, Thread* thread) {
 #pragma region Lock Functions
 
 Thread* getThreadHoldingLock(const char* lockId) {
-    return (Thread *) GET_FROM_MAP(const char*, lockToHoldingThread, lockId);
+    return (Thread*)GET_FROM_MAP(const char*, lockToHoldingThread, lockId);
 }
 
 #pragma endregion
--- answer/thread.cpp
+++ answer/thread.cpp
@@ -9,47 +9,55 @@
 #include "Thread.h"
 #include "thread_lock.h"
 
-
-// invariant: readyList is sorted by priority in descending order
+/**
+ * @brief A list of ready-to-run threads sorted by priority in descending order.
+ * invariant: readyList is sorted by priority in descending order
+ */
 const char* readyList = NULL;
 
 /**
- * @brief The map stores information about asleep threads and when each thread should wake up
- * Map's key: int, tick.
- * Map's value: thread to resume at that tick.
- * todo: what if we have more than one threads to resume at certain tick? Looks like we need a list for the val.
+ * @brief The map stores information about asleep threads and when each thread
+ * should wake up Map's key: int, tick. Map's value: thread to resume at that
+ * tick. todo: what if we have more than one threads to resume at certain tick?
+ * Looks like we need a list for the val.
  */
 const char* wokentickToThreads = NULL;
 
 /**
- * @brief A map whose key is lockId and the key's value is the thread holding the lock.
- *        If no thread is holding the key/lock, the key's value should be NULL.
+ * @brief A map whose key is lockId and the key's value is the thread holding
+ * the lock. If no thread is holding the key/lock, the key's value should be
+ * NULL.
  */
 const char* lockToHoldingThread = NULL;
 
 /**
- * @brief A map whose key is lockId and the key's value is a list of threads waiting for the lock.
+ * @brief A map whose key is lockId and the key's value is a list of threads
+ * waiting for the lock.
  */
 const char* lockToWaitingThreads = NULL;
 
 /**
- * @brief A map whose key is a thread and the key's value is a list of locks the key is holding.
+ * @brief A map whose key is a thread and the key's value is a list of locks the
+ * key is holding.
  */
 const char* threadToHoldingLocks = NULL;
 
 /**
- * @brief A map whose key is a thread and the key's value is the lock the key is waiting for.
+ * @brief A map whose key is a thread and the key's value is the lock the key is
+ * waiting for.
  */
 const char* threadToWaitingLock = NULL;
 
 // return true if v1->priority is greater than v2->prioirty
-bool priorityCompare(void *v1, void *v2) {
-    Thread *t1 = (Thread *) v1;
-    Thread *t2 = (Thread *) v2;
-    // method sortList sorts given list in ascending order if we implement the comparator it says (v1 <= v2 return true),
-    // but here we want sort readyList in descending order so the first element is always of the highest priority
-    // therefore, we need the opposite of the description, which is return false for v1 <= v2
-    // return !(t1->priority <= t2->priority);
+bool priorityCompare(void* v1, void* v2) {
+    Thread* t1 = (Thread*)v1;
+    Thread* t2 = (Thread*)v2;
+    // method sortList sorts given list in ascending order if we implement the
+    // comparator it says (v1 <= v2 return true), but here we want sort
+    // readyList in descending order so the first element is always of the
+    // highest priority therefore, we need the opposite of the description,
+    // which is return false for v1 <= v2 return !(t1->priority <=
+    // t2->priority);
     return t1->priority > t2->priority;
 }
 
@@ -71,14 +79,15 @@ Thread* createAndSetThreadToRun(const char* name,
     addToList(readyList, (void*)ret);
 
     // char line[1024];
-    // sprintf(line, "now the list contains %d threads:\n", listSize(readyList));
-    // for (int i = 0; i < listSize(readyList); i++) {
+    // sprintf(line, "now the list contains %d threads:\n",
+    // listSize(readyList)); for (int i = 0; i < listSize(readyList); i++) {
     //     Thread *curThread = (Thread*) listGet(readyList, i);
     //     sprintf(line, "wzw %s\n", curThread->name);
     // }
     // verboseLog(line);
 
-    // this way whenever a new thread is added, the thread at index 0 will always be the one with the highest priority
+    // this way whenever a new thread is added, the thread at index 0 will
+    // always be the one with the highest priority
     sortList(readyList, priorityCompare);
     return ret;
 }
@@ -96,14 +105,9 @@ Thread* nextThreadToRun(int currentTick) {
     sprintf(line, "[nextThreadToRun] Showing readyList...\n");
     verboseLog(line);
     for (int i = 0; i < listSize(readyList); i++) {
-        Thread* t = (Thread *) listGet(readyList, i);
-        sprintf(
-            line,
-            "[nextThreadToRun] %d - thread %s - priority %d\n",
-            i,
-            t->name,
-            t->priority
-        );
+        Thread* t = (Thread*)listGet(readyList, i);
+        sprintf(line, "[nextThreadToRun] %d - thread %s - priority %d\n", i,
+                t->name, t->priority);
         verboseLog(line);
     }
     sprintf(line, "[nextThreadToRun] Showed readyList...\n");
@@ -113,21 +117,27 @@ Thread* nextThreadToRun(int currentTick) {
     // extract them from wokentickToThreads and add them into readyList
     bool needResumeThread = MAP_CONTAINS(int, wokentickToThreads, currentTick);
     if (needResumeThread) {
-        Thread *threadToResume = (Thread *) REMOVE_FROM_MAP(int, wokentickToThreads, currentTick);
-        sprintf(line, "[nextThreadToRun] add thread %s from wokentickToThreads to readyList\n", threadToResume->name);
+        Thread* threadToResume =
+            (Thread*)REMOVE_FROM_MAP(int, wokentickToThreads, currentTick);
+        sprintf(line,
+                "[nextThreadToRun] add thread %s from wokentickToThreads to "
+                "readyList\n",
+                threadToResume->name);
         verboseLog(line);
         addToList(readyList, threadToResume);
         sortList(readyList, priorityCompare);
     }
 
-    sprintf(line, "[nextThreadToRun] there are %d threads in readyList\n", listSize(readyList));
+    sprintf(line, "[nextThreadToRun] there are %d threads in readyList\n",
+            listSize(readyList));
     verboseLog(line);
     if (listSize(readyList) == 0) {
         return NULL;
     }
-        
+
     Thread* ret = NULL;
-    // choose 0 based on the assumption that our thread list is always sorted in descending order of priority
+    // choose 0 based on the assumption that our thread list is always sorted in
+    // descending order of priority
     int threadIndex = 0;
     do {
         sprintf(line, "[nextThreadToRun] trying thread index %d\n",
@@ -148,7 +158,8 @@ Thread* nextThreadToRun(int currentTick) {
             sprintf(line, "[nextThreadToRun] here wzw %d\n", __LINE__);
             verboseLog(line);
             if (retIsBlocked) {
-                sprintf(line, "[nextThreadToRun] thread %s is blocked wzw\n", ret->name);
+                sprintf(line, "[nextThreadToRun] thread %s is blocked wzw\n",
+                        ret->name);
                 verboseLog(line);
                 ret = NULL;
             }
@@ -157,7 +168,8 @@ Thread* nextThreadToRun(int currentTick) {
         }
 
         threadIndex++;
-    } while (listSize(readyList) > 0 && ret == NULL && threadIndex < listSize(readyList));
+    } while (listSize(readyList) > 0 && ret == NULL &&
+             threadIndex < listSize(readyList));
 
     // removeFromList(readyList, (void*) ret);
     // addToList(readyList, ret);
@@ -185,7 +197,7 @@ void shutdownCallback() {
 // 1. the current thread has been paused
 // 2. the current thread will not be woken up until numTicks have passed
 int tickSleep(int numTicks) {
-    Thread *curThread = getCurrentThread();
+    Thread* curThread = getCurrentThread();
     // PRECONDITION: the first thread in readyList must be
     // the current running thread returned by getCurrentThread()
     removeFromListAtIndex(readyList, 0);
@@ -203,7 +215,7 @@ int tickSleep(int numTicks) {
 
 // POSTCONDITION: readyList is sorted by priority (descending)
 void setMyPriority(int priority) {
-    Thread *thread = getCurrentThread();
+    Thread* thread = getCurrentThread();
     thread->priority = priority;
 
     // With the precondition that readyList is always sorted
